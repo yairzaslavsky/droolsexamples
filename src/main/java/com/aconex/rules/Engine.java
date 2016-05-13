f01ea7f6a01319502ad6b28b0a76f42d24bfdaf6
Yair Zaslavsky <yzaslavsky@gmail.com>
2016-05-13 11:13:57 +1000

    Example 1:
    Removing all recepients of organizaion ACME

---
+++ src/main/java/com/aconex/rules/Engine.java
@@ -0,0 +1,71 @@
+package com.aconex.rules;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.compiler.DroolsError;
+import org.drools.compiler.DroolsParserException;
+import org.drools.compiler.PackageBuilder;
+import org.drools.compiler.PackageBuilderErrors;
+
+public class Engine {
+
+
+    public WorkingMemory createRulesSession(InputStream rulesDataStream) throws IOException, DroolsParserException {
+
+        InputStreamReader reader = new InputStreamReader(rulesDataStream);
+        PackageBuilder builder = new PackageBuilder();
+        builder.addPackageFromDrl(reader);
+        RuleBase ruleBase = addRulesToWorkingMemory(builder);
+        WorkingMemory workingMemory = ruleBase.newStatefulSession();
+        return workingMemory;
+    }
+
+    public WorkingMemory createRulesSessionFromResource(String resourceName) throws IOException, DroolsParserException {
+        return createRulesSession(this.getClass().getResourceAsStream(resourceName));
+
+    }
+
+    public <T> void attachObjectToSession(WorkingMemory session, T object) {
+        session.insert(object);
+    }
+
+
+    private Reader getRuleFileAsReader(String ruleFile) {
+        InputStream resourceAsStream = getClass().getResourceAsStream(ruleFile);
+
+        return new InputStreamReader(resourceAsStream);
+    }
+
+    private RuleBase addRulesToWorkingMemory(PackageBuilder packageBuilder) {
+        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+        org.drools.rule.Package rulesPackage = packageBuilder.getPackage();
+        ruleBase.addPackage(rulesPackage);
+
+        return ruleBase;
+    }
+
+
+    private void assertNoRuleErrors(PackageBuilder packageBuilder) {
+        PackageBuilderErrors errors = packageBuilder.getErrors();
+
+        if (errors.getErrors().length > 0) {
+            StringBuilder errorMessages = new StringBuilder();
+            errorMessages.append("Found errors in package builder\n");
+            for (int i = 0; i < errors.getErrors().length; i++) {
+                DroolsError errorMessage = errors.getErrors()[i];
+                errorMessages.append(errorMessage);
+                errorMessages.append("\n");
+            }
+            errorMessages.append("Could not parse knowledge");
+
+            throw new IllegalArgumentException(errorMessages.toString());
+        }
+    }
+
+}
--- src/test/java/com/example/DroolsTest.java
+++ src/test/java/com/example/DroolsTest.java
@@ -1,115 +1,45 @@
 
 package com.example;
 
+import static com.sample.Organizations.instance;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
+import com.aconex.rules.Engine;
 import com.sample.Example;
-import org.drools.RuleBase;
-import org.drools.RuleBaseFactory;
+import com.sample.Mail;
 import org.drools.WorkingMemory;
-import org.drools.compiler.DroolsError;
 import org.drools.compiler.DroolsParserException;
-import org.drools.compiler.PackageBuilder;
-import org.drools.compiler.PackageBuilderErrors;
-import org.drools.rule.Package;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class DroolsTest {
 
-    @Test
-    public void shouldNotFireHelloWorldIfAgeIsNotMinimal() throws IOException, DroolsParserException {
-        RuleBase ruleBase = initialiseDrools();
-        WorkingMemory workingMemory = initializeMessageObjects(ruleBase ,"Hello", 6);
-        int expectedNumberOfRulesFired = 0;
+    private Example.Message helloMessage;
 
-        int actualNumberOfRulesFired = workingMemory.fireAllRules();
+    @BeforeClass
+    public static void before() {
+        instance().addRecepient("ACME", "Yair");
+        instance().addRecepient("ACME", "Yanhui");
+        instance().addRecepient("PURPLE", "Dmitriy");
 
-        assertThat(actualNumberOfRulesFired, is(expectedNumberOfRulesFired));
     }
 
     @Test
-    public void shouldFireHelloWorldIfAgeIsAboveMinimal() throws IOException, DroolsParserException {
-        RuleBase ruleBase = initialiseDrools();
-        WorkingMemory workingMemory = initializeMessageObjects(ruleBase, "Hello", 19);
-        int expectedNumberOfRulesFired = 1;
-
-        int actualNumberOfRulesFired = workingMemory.fireAllRules();
-
-        assertThat(actualNumberOfRulesFired, is(expectedNumberOfRulesFired));
+    public void shouldMeow() throws IOException, DroolsParserException {
+        Engine engine = new Engine();
+        WorkingMemory session = engine.createRulesSessionFromResource("/remove_acme_org_recepients.drl");
+        Mail mail = new Mail();
+        mail.addRecepient("Yair");
+        mail.addRecepient("Yanhui");
+        mail.addRecepient("Dmitriy");
+        engine.attachObjectToSession(session, mail);
+        session.fireAllRules();
+        assertThat(mail.getRecepients().size(), is(1));
+        assertThat(mail.getRecepients().get(0), is("Dmitriy"));
     }
 
-
-    private RuleBase initialiseDrools() throws IOException, DroolsParserException {
-        PackageBuilder packageBuilder = readRuleFiles();
-        return addRulesToWorkingMemory(packageBuilder);
-    }
-
-    private PackageBuilder readRuleFiles() throws DroolsParserException, IOException {
-        PackageBuilder packageBuilder = new PackageBuilder();
-
-        File f = new File("/sample.drl");
-        System.out.println(f.exists());
-
-        String ruleFile = "/sample.drl";
-        Reader reader = getRuleFileAsReader(ruleFile);
-        packageBuilder.addPackageFromDrl(reader);
-
-        assertNoRuleErrors(packageBuilder);
-
-        return packageBuilder;
-    }
-
-    private Reader getRuleFileAsReader(String ruleFile) {
-        InputStream resourceAsStream = getClass().getResourceAsStream(ruleFile);
-
-        return new InputStreamReader(resourceAsStream);
-    }
-
-    private RuleBase addRulesToWorkingMemory(PackageBuilder packageBuilder) {
-        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
-        Package rulesPackage = packageBuilder.getPackage();
-        ruleBase.addPackage(rulesPackage);
-
-        return ruleBase;
-    }
-
-    private void assertNoRuleErrors(PackageBuilder packageBuilder) {
-        PackageBuilderErrors errors = packageBuilder.getErrors();
-
-        if (errors.getErrors().length > 0) {
-            StringBuilder errorMessages = new StringBuilder();
-            errorMessages.append("Found errors in package builder\n");
-            for (int i = 0; i < errors.getErrors().length; i++) {
-                DroolsError errorMessage = errors.getErrors()[i];
-                errorMessages.append(errorMessage);
-                errorMessages.append("\n");
-            }
-            errorMessages.append("Could not parse knowledge");
-
-            throw new IllegalArgumentException(errorMessages.toString());
-        }
-    }
-
-    private WorkingMemory initializeMessageObjects(RuleBase ruleBase, String msg, int age) {
-        WorkingMemory workingMemory = ruleBase.newStatefulSession();
-
-        createHelloWorld(workingMemory, msg, age);
-
-        return workingMemory;
-    }
-
-    private void createHelloWorld(WorkingMemory workingMemory, String msg, int age) {
-        Example.Message helloMessage = new Example.Message();
-        helloMessage.setMessage(msg);
-        helloMessage.setAge(age);
-        workingMemory.insert(helloMessage);
-    }
 }
